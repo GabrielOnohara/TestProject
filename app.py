4bdb19b97c2c14c956cee7036bafc24bb98ae5dc
Gabriel Onohara <gabakio11@gmail.com>
2025-02-07 20:05:37 -0300

    using 1 ot n relationship

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+from seeds import seed_users, seed_pets
+seed_users()
+seed_pets()
+
 import falcon
-from sqlalchemy.orm import scoped_session
+import json
+from sqlalchemy.orm import scoped_session, joinedload
+
 from db import Session
-from models import User
-from seeds import seed_users
+from models import User, Pet
+
+
+class RootResource:
+    def on_get(self, req, resp):
+        resp.media = {'message': 'Welcome to Falcon API'}
 
 class UserResource:
     def on_get(self, req, resp):
@@ -21,7 +31,35 @@ class UserResource:
         resp.media = {'id': new_user.id, 'name': new_user.name, 'email': new_user.email}
         session.close()
 
+class PetResource:
+    def on_get(self, req, resp):
+        session = Session()
+        pets = session.query(Pet).all()
+        resp.media = [{'id': pet.id, 'name': pet.name, 'species': pet.species} for pet in pets]
+        session.close()
+
+class UserPetsResource:
+    def on_get(self, req, resp):
+        session = Session()
+        users = session.query(User).options(joinedload(User.pets)).all()
+        users_resp = []
+        for user in users:
+            user_obj = {'id': user.id, 'name': user.name, 'email': user.email, 'pets': []}
+            for pet in user.pets:
+                user_obj['pets'].append({
+                    'name': pet.name,
+                    'species': pet.species
+                })
+            users_resp.append(user_obj)
+        print(users_resp)
+        resp.media = users_resp
+        session.close()
+
+
 # app = falcon.asgi.App()
-seed_users()
 app = falcon.App()
+
+app.add_route('/', RootResource())
 app.add_route('/users', UserResource())
+app.add_route('/pets', PetResource())
+app.add_route('/user_pets', UserPetsResource())
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from db import get_engine
 
 Base = declarative_base()
@@ -10,6 +11,16 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
+    pets = relationship("Pet", back_populates="owner")
+
+class Pet(Base):
+    __tablename__ = 'pets'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    species = Column(String, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    owner = relationship("User", back_populates="pets")
 
 engine = get_engine()
 
