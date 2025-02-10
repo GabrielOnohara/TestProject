cc9c8431744f31e3f790495aabb9fe1e20325462
Gabriel Onohara <gabakio11@gmail.com>
2025-02-10 18:31:07 -0300

    testing more methods

--- controllers/users.py
+++ controllers/users.py
@@ -1,3 +1,4 @@
+import falcon
 from db import Session
 from models import User
 
@@ -16,4 +17,30 @@ class UserResource:
         session.commit()
         resp.status = falcon.HTTP_201
         resp.media = {'id': new_user.id, 'name': new_user.name, 'email': new_user.email}
+        session.close()
+
+    def on_put(self, req, resp, user_id):
+        session = Session()
+        data = req.media
+        user = session.query(User).get(user_id)
+        if user:
+            user.name = data['name']
+            user.email = data['email']
+            session.commit()
+            resp.media = {'id': user.id, 'name': user.name, 'email': user.email}
+        else:
+            resp.status = falcon.HTTP_404
+            resp.media = {'message': 'User not found'}
+        session.close()
+
+    def on_delete(self, req, resp, user_id):
+        session = Session()
+        user = session.query(User).get(user_id)
+        if user:
+            session.delete(user)
+            session.commit()
+            resp.status = falcon.HTTP_204
+        else:
+            resp.status = falcon.HTTP_404
+            resp.media = {'message': 'User not found'}
         session.close()
--- routes.py
+++ routes.py
@@ -3,5 +3,6 @@ from controllers import *
 
 app.add_route('/', RootResource())
 app.add_route('/users', UserResource())
+app.add_route('/users/{user_id}', UserResource())
 app.add_route('/pets', PetResource())
 app.add_route('/user_pets', UserPetsResource())
